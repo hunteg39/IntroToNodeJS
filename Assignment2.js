36b5f15cc6dc3258f05db17204ef11d953d2efca
hunteg39 <hunteg39@uwosh.edu>
2020-11-25 19:38:41 -0600

    added package json, Port changes to web server and proc file

--- Assignment2.js
+++ Assignment2.js
@@ -3,6 +3,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+const PORT = process.env.PORT || 5000;
 
 http.createServer(function(req, res) {
 	var q = url.parse(req.url, true);
@@ -21,6 +22,6 @@ fs.readFile(filename, function(err, data) {
 		res.write(data);
 		return res.end();
 	});
-}).listen(8080);
+}).listen(PORT);
 
-console.log("Server Listening on Port 8080...");
+console.log("Server Listening on Assigned PORT...");
