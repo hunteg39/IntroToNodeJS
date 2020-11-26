5dfcecceead540d3adf1af38a04b27e969289a32
hunteg39 <hunteg39@uwosh.edu>
2020-11-25 18:53:11 -0600

    initial commit

---
+++ Quiz.js
@@ -0,0 +1,29 @@
+console.log('Hello World');
+//Grant Hunter IS350 Quiz 6
+
+var a = 8 + "8"
+console.log(a);
+
+//var email = 'ivor@uwosh.edu'; 
+//var emailDomain = 'uwosh.edu';
+//var result = email.includes(emailDomain);
+//console.log(result);
+
+//let age = 20; 
+//let result = age > 18 ? 'Great' : 'Not so great'; 
+//console.log(result);
+
+let result = 0;
+
+for(let i = 0; i < 8; i++){ 
+   result = result + i;
+}
+
+console.log(result);
+
+let k = [];
+
+k[0] = 1;
+k[1] = true;
+k[2] = 'Blue';
+console.log(k);
